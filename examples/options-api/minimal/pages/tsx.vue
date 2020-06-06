4c657e4bc7b96451deb56e5219bce29dd2fc6eb8
yamaya_tomohiro <yamaya.tomohiro@adways.net>
2020-06-06 09:16:50 +0900

    chore(example): add reproduction for tsx error

---
+++ examples/options-api/minimal/pages/tsx.vue
@@ -0,0 +1,18 @@
+<script lang="tsx">
+import Vue, { VNode } from 'vue'
+
+export default Vue.extend({
+  data () {
+    const message: string = 'This is a message'
+
+    return {
+      message
+    }
+  },
+  render (): VNode {
+    return (
+      <div>{this.message} (tsx)</div>
+    )
+  }
+})
+</script>
